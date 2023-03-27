4811d9538697fab9fc5047490692884726f25254
jessicahattig <jessicahattig@gmail.com>
2023-03-27 16:31:44 -0700

    BUGFIX still working on result output.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,14 @@
 window.onload = function() {
+console.log("page loaded");
   const form = document.querySelector("form");
-  const blamBtn = document.querySelector("#button");
-  const results = document.querySelector("#results-list");
+  console.log(form);
+  const blamBtn = document.querySelector("button");
+  console.log(blamBtn);
+  const results = document.querySelector("results-list");
+  console.log(results);
 
   blamBtn.addEventListener("click", function(){
+  console.log(results);
     const favoriteThings = [
       document.querySelector('#favorite1').value,
       document.querySelector('#favorite2').value,
@@ -11,9 +16,10 @@ window.onload = function() {
       document.querySelector('#favorite4').value,
       document.querySelector('#favorite5').value,
     ]
+    console.log(favoriteThings[1]);
 
-  const li_1 = document.createElement("li");
-  li_1.append(favoriteThings[0]);
+  const li_1 = document.createElement("li")
+  li_1.append(toString(favoriteThings[0]))
   results.append(li_1);
   })
 }
